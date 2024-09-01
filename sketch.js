95ebc9ce59cf8ece8c73c44d7bbd7d1bb849f0d1
Evelin Mariane <moreiraeve05@gmail.com>
2024-09-01 15:09:17 +0000

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function setup() {
+    createCanvas(400, 400);
+    background("yellow");
+   }
+  
+  
+  
+  
+  function draw() {
+    stroke("brown");
+    fill("red");
+  
+  if(mouseIsPressed){
+    circle(mouseX, mouseY, 20, 35);  
+    }
+  
+  }
